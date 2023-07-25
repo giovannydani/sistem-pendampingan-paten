a52e94fcd39a75eef23522e2a65588575964e0f3
Giovanny Dani Saputra <giovannysaputra14@gmail.com>
2023-07-26 04:39:51 +0700

    sidebar cleaning

--- resources/views/partial/sidebar.blade.php
+++ resources/views/partial/sidebar.blade.php
@@ -49,31 +49,6 @@
             <span>Ajuan</span>
             </a>
         </li>
-        {{-- <li class="sidebar-item {{ request()->routeIs('admin.ajuan.*') ? 'active' : '' }}">
-            <a href="{{ route('admin.ajuan.index') }}" class="sidebar-link">
-            <i class="bi bi-grid-fill"></i>
-            <span>Ajuan</span>
-            </a>
-        </li>
-        
-        <li class="sidebar-item mt-5 {{ request()->routeIs('admin.parameter.holder.*') ? 'active' : '' }}">
-            <a href="{{ route('admin.parameter.holder.index') }}" class="sidebar-link">
-                <i class="bi bi-grid-fill"></i>
-                <span>Default Holder</span>
-            </a>
-        </li>
-        <li class="sidebar-item {{ request()->routeIs('admin.template.*') ? 'active' : '' }}">
-            <a href="{{ route('admin.template.index') }}" class="sidebar-link">
-                <i class="bi bi-grid-fill"></i>
-                <span>Template</span>
-            </a>
-        </li>
-        <li class="sidebar-item {{ request()->routeIs('admin.application-type.*') ? 'active' : '' }}">
-            <a href="{{ route('admin.application-type.index') }}" class="sidebar-link">
-                <i class="bi bi-grid-fill"></i>
-                <span>Tipe Ajuan</span>
-            </a>
-        </li> --}}
         @if (auth()->user()->role->isSuperAdmin())
             <li class="sidebar-item {{ request()->routeIs('admin.manage-admin.*') ? 'active' : '' }}">
                 <a href="{{ route('admin.manage-admin.index') }}" class="sidebar-link">
@@ -85,7 +60,6 @@
     @endif
     
     
-        {{-- @dump(auth()->user()->role->isAdmin()) --}}
         @if (auth()->user()->role->isAdmin())
         <li class="sidebar-item mt-5 {{ request()->routeIs('admin.profile.*') ? 'active' : '' }}">
             <a href="{{ route('admin.profile.index') }}" class="sidebar-link">
@@ -108,27 +82,6 @@
             <span style="color: #fff">Log Out</span>
             </a>
         </li>
-
-      {{-- @if (request('is_super_admin') || request('is_admin'))    
-        @if (request()->routeIs('user.*'))
-            <li class="sidebar-item mt-5">
-            <a href="{{ route('admin.dashboard') }}" class="sidebar-link" style="background-color: red; color: white">
-                <i class="fa-solid fa-arrow-right" style="color: white"></i>
-                <span>Go To Admin Panel</span>
-            </a>
-            </li>
-        @endif
-
-        @if (request()->routeIs('admin.*'))
-            <li class="sidebar-item mt-5">
-            <a href="{{ route('user.dashboard') }}" class="sidebar-link" style="background-color: red; color: white">
-                <i class="fa-solid fa-arrow-right" style="color: white"></i>
-                <span>Go To User Panel</span>
-            </a>
-            </li>
-        @endif
-      @endif --}}
-
 
     </ul>
   </div>
